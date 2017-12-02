478dbcc58b6eaaed5a206aafaebe0e2a6042d59d
April Mintac Pineda <aprilmintacpineda@gmail.com>
2017-12-02 13:06:43 +0800

    initial pages and router setup

--- resources/assets/js/App.js
+++ resources/assets/js/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Switch, Route, Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
+// pages
+import Dashboard from './pages/Dashboard';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
-			<p>App Component!</p>
+			<Router history={history}>
+				<Switch>
+					<Route exact path="/" component={Dashboard} />
+				</Switch>
+			</Router>
 		);
 	}
 }
---
+++ resources/assets/js/pages/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Dashboard!</div>
+    );
+  }
+}
